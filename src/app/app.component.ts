793535e3753c194a8d9becc4cc759cf448718361
mathisGarberg <Mathis.Garberg@gmail.com>
2021-03-18 22:33:51 +0100

    feat: added dynamic footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { WebFlowPaths } from 'src/core/enums/paths';
 })
 export class AppComponent {
   title = 'mathis-page';
+  currentYear = new Date().getFullYear();
   logo = '../assets/images/logo-resized.png';
   navigation = [
     { link: WebFlowPaths.About, label: 'app.menu.about' },
@@ -17,5 +18,32 @@ export class AppComponent {
   navigationSideMenu = [
     ...this.navigation,
     { link: WebFlowPaths.Settings, label: 'app.menu.settings' },
+  ];
+  mediaContents = [
+    {
+      link: 'https://www.linkedin.com/in/mathis-garberg-451672144/',
+      name: 'LinkedIn',
+      icon: 'linkedin',
+    },
+    {
+      link: 'https://www.github.com/mathisGarberg',
+      name: 'Github',
+      icon: 'github',
+    },
+    {
+      link: 'https://www.medium.com/@mathis.garberg',
+      name: 'Medium',
+      icon: 'medium-m',
+    },
+    {
+      link: 'https://stackoverflow.com/users/4228322/mathis-garberg',
+      name: 'Stack Overflow',
+      icon: 'stack-overflow',
+    },
+    {
+      link: 'https://unsplash.com/@pi_mathis',
+      name: 'Unsplash',
+      icon: 'unsplash',
+    },
   ];
 }
