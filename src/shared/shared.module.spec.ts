4524ce8d4d379e7cd842a5cf79811fa13ae0fc23
mathisGarberg <Mathis.Garberg@gmail.com>
2021-03-20 01:22:04 +0100

    feat: added patch aliases

---
+++ src/shared/shared.module.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { Store } from '@ngrx/store';
+import { SharedModule } from './shared.module';
+
+describe(`SharedModule`, () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [SharedModule],
+    });
+  });
+
+  it(`should provide 'Store' service`, () => {
+    expect(() => TestBed.inject(Store)).toBeTruthy();
+  });
+});
